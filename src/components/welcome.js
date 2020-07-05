3bf4ac4a93da5c2dbef7b73c9736efc0fd5310ba
nikhil.karkra@rmit.edu.au <nikhil.karkra@rmit.edu.au>
2020-07-05 18:56:19 +1000

    Initial commit from gatsby: (https://github.com/gatsbyjs/gatsby-starter-default.git)

---
+++ src/components/welcome.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import './style.css'
+const NotFoundPage = () => (
+    <Layout>
+        <div class="bgimg">
+            <div class="topleft">
+                <p>Logo</p>
+            </div>
+            <div class="middle">
+                <h1>COMING SOON</h1>
+                <hr/>
+                    <p>35 days left</p>
+            </div>
+            <div class="bottomleft">
+                    <p>Some text</p>
+            </div>
+        </div>
+        
+    </Layout>
+)
+
+export default NotFoundPage
+
