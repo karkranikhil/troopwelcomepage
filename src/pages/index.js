74f3add07db9cb4fbc06183e75ef61262d740c9e
nikhil.karkra@rmit.edu.au <nikhil.karkra@rmit.edu.au>
2020-07-05 21:28:30 +1000

    first

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout.js"
 import SEO from "../components/seo.js"
-import Welcome from "../components/Welcome.js"
+import Welcome from "../components/welcome.js"
 const IndexPage = () => (
   <Layout>
     <SEO title="Salesforce Troop" />
